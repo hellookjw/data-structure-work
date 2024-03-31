cd4b28b695c3d2b6321d0c44a2f7e30d7622826b
hellookjw <127330308+hellookjw@users.noreply.github.com>
2024-03-31 23:01:43 +0900

    Update 과제5.c

--- 과제5.c
+++ 과제5.c
@@ -6,7 +6,7 @@ void hanoi(int n, char from, char to, char aux) {
         return;
     }
     hanoi(n - 1, from, aux, to);
-    printf("원반 %d을(를) %c에서 %c로 옮김\n", n, from, to);
+    printf("원반 %d을 %c에서 %c로 옮김\n", n, from, to);
     hanoi(n - 1, aux, to, from);
 }
 
